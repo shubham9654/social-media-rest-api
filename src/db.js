98ef6ca9fe8d8a9db934201c6e4b1a08818023c0
Mivan Urvish <officemivan@gmail.com>
2024-05-22 10:50:19 +0530

    added app and db files

---
+++ src/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+	try {
+		await mongoose.connect(process.env.MONGO_DB_URI);
+		console.log(`\n MongoDB connected !! DB HOST ON PORT: ${port}`);
+	} catch (err) {
+		console.log(err);
+	};
+};
+
+module.exports = start;
